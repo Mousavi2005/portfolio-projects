02b388ef5edac64fed291ae9473e8eccc9f08eec
Mousavi <161970487+Mousavi2005@users.noreply.github.com>
2025-04-22 20:49:35 +0330

    Upload project
    uploading social link project

---
+++ Social-Links/index.js
@@ -0,0 +1,15 @@
+function gitHub() {
+    window.open(url = 'https://github.com/Mousavi2005/Frontend-Projects')
+}
+
+function linkedin() {
+    window.open(url='https://www.linkedin.com/in/seyed-mohammad-amin-mousavi-900971352?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app')
+}
+
+function x(){
+    window.open(url='https://x.com/Mousavi2005cs?s=09')
+}
+
+function instagram() {
+    window.open(url = 'https:/www.instagram.com')
+}
