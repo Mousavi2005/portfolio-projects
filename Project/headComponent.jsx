0475a98af65f34bffefaa556d3d1db4de7ccda2b
Mousavi <161970487+Mousavi2005@users.noreply.github.com>
2025-04-22 21:05:15 +0330

    Uploading project
    uploading react-facts project

---
+++ Project/headComponent.jsx
@@ -0,0 +1,8 @@
+export default function Head(){
+    return (
+        <div className="nav-bar">
+            <img src='./src/assets/react.svg' alt="" />
+            <span className="logo-name">ReactFacts</span>
+        </div>
+    )
+}
---
+++ Project/mainComponent.jsx
@@ -0,0 +1,15 @@
+export default function Main() {
+    return (
+            <main>
+                <h1 className="main-section-heading">Fun facts about React</h1>
+                <ul className="list-content">
+                    <li className="list-item">was first released in 2013</li>
+                    <li className="list-item">Was originaly created by jordan walke</li>
+                    <li className="list-item">Has well over 200k stars on github</li>
+                    <li className="list-item">Is maintained by Meta</li>
+                    <li className="list-item">Powers thousands of apps</li>
+                </ul>
+            </main>
+
+    )
+}
