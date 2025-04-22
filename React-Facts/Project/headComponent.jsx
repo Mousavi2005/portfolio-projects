2441ee37e81c1557796e69f433c99dc4e4fd89d4
Mousavi <161970487+Mousavi2005@users.noreply.github.com>
2025-04-22 21:04:25 +0330

    Delete React-Facts/Project directory

--- React-Facts/Project/headComponent.jsx
+++
@@ -1,8 +0,0 @@
-export default function Head(){
-    return (
-        <div className="nav-bar">
-            <img src='./src/assets/react.svg' alt="" />
-            <span className="logo-name">ReactFacts</span>
-        </div>
-    )
-}
