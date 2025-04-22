fa7e9a2c39e0764353c37ca576af4b4b33dbc572
Mousavi <161970487+Mousavi2005@users.noreply.github.com>
2025-04-22 21:02:54 +0330

    Uploading project
    uploading react-facts project

---
+++ React-Facts/Project/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Head from '../headComponent'
+import Main from '../mainComponent'
+
+function App() {
+  return (
+    <>
+      <Head />
+      <Main />
+    </>
+  )
+}
+
+export default App
